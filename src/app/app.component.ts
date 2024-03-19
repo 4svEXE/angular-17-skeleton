d6d3cd8ac6e70393122c0e1f642d66d7e148434d
4SVEXE <4sv.exe@gmail.com>
2024-03-19 21:41:49 +0200

    add: login service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { AuthenticationService } from './core/services/authentication.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-skeleton';
+
+  constructor(private authService: AuthenticationService) {}
+
+  testUser = {
+    name: 'Jimm Helpert',
+    email: 'solGoodman@gamail.com',
+    password: 'wtf12345',
+  };
+
+  onSubmit() {
+    this.authService
+      .registerAndLogin(this.testUser)
+      .subscribe((userId) => {
+        console.log('userId', userId);
+
+        // this.router.navigate(['user/' + userId]);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, PagesModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule, SharedModule, PagesModule],
+  providers: [
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
